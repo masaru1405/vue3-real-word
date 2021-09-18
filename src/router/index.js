7de96033d908c8f64746aed14e7da9636bc83945
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2021-09-18 14:27:52 -0300

    primeiro commit

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import EventList from '../views/EventList.vue'
+import EventDetails from '@/views/EventDetails.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'EventList',
+    component: EventList
+  },
+  {
+    path: '/event/:id',
+    name: 'EventDetails',
+    props: true,
+    component: EventDetails
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
